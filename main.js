af5ff1c8e1572b09cb8ee61558930555d8397051
manasgautam1 <69212064+manasgautam1@users.noreply.github.com>
2020-10-16 19:51:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+document.getElementById('signUp').addEventListener('click', function() {
+	container.classList.add("right-panel-active");
+});
+ document.getElementById('signIn').addEventListener('click', function() {
+	container.classList.remove("right-panel-active");
+});
